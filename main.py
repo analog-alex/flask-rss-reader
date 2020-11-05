9044a1438d9f05af99af1745109d8daa30b996c1
analog-alex <miguel.carvalho@talkdesk.com>
2020-11-05 20:54:05 +0000

    Remove CORS

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from src.controllers.index_controller import index_controller
 from src.controllers.feeds_controller import feeder_controller
 from src.repository.elasticsearch import ping
 
-
 ########################################
 # entry point for flask app
 # - Flask version (1.1.2)
@@ -11,6 +10,15 @@ from src.repository.elasticsearch import ping
 #
 ########################################
 app = Flask(__name__)
+
+
+# let's allow request from anywhere
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
 
 # register controllers
 app.register_blueprint(index_controller)
